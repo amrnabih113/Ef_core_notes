c10b434494ec76b03b4838827f508bd841dedaeb
Amr Nabih <amrnabih112@gmail.com>
2025-10-01 18:13:35 +0300

    Transaction Example

--- EF_Core/Program.cs
+++ EF_Core/Program.cs
@@ -71,13 +71,47 @@ namespace EF_Core
             //}
 
             // Query employees with salary > 50000
+            //using (var context = new AppDbContext())
+            //{
+            //    var highSalaryEmps = context.Employee.Where(e => e.salary > 50000).ToList();
+            //    foreach (var emp in highSalaryEmps)
+            //    {
+            //        Console.WriteLine(emp);
+            //        Console.WriteLine("--------------------------------------------------");
+            //    }
+            //}
+
+            // transaction example
             using (var context = new AppDbContext())
             {
-                var highSalaryEmps = context.Employee.Where(e => e.salary > 50000).ToList();
-                foreach (var emp in highSalaryEmps)
+                using var transaction = context.Database.BeginTransaction();
+                try
                 {
-                    Console.WriteLine(emp);
-                    Console.WriteLine("--------------------------------------------------");
+                    var emp1 = new Employee
+                    {
+                        name = "Transaction Emp 1",
+                        salary = 55000,
+                        age = 28
+                    };
+                    context.Employee.Add(emp1);
+                    context.SaveChanges();
+                    var emp2 = new Employee
+                    {
+                        name = "Transaction Emp 2",
+                        salary = 65000,
+                        age = 32
+                    };
+                    context.Employee.Add(emp2);
+                    context.SaveChanges();
+                    // Commit transaction if both inserts succeed
+                    transaction.Commit();
+                    Console.WriteLine("Transaction committed successfully.");
+                }
+                catch (Exception ex)
+                {
+                    // Rollback transaction if any insert fails
+                    transaction.Rollback();
+                    Console.WriteLine("Transaction rolled back due to an error: " + ex.Message);
                 }
             }
         }
